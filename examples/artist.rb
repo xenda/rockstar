92d40989dec7c9f1afa8418f2d317110b0c07418
Nick Plante <nap@zerosum.org>
2011-05-28 22:24:04 -0400

    add artist events example

--- examples/artist.rb
+++ examples/artist.rb
@@ -4,11 +4,17 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'rocksta
 # You can find them here : http://www.lastfm.de/api/account
 Rockstar.lastfm = YAML.load_file(File.join(File.dirname(__FILE__), 'lastfm.yml'))
 
-artist = Rockstar::Artist.new('Madonna')
+artist = Rockstar::Artist.new('Metallica')
 
 puts 'Top Tracks'
 puts "=" * 10
 artist.top_tracks.each { |t| puts "#{t.name}" }
+
+puts
+
+puts 'Events'
+puts '=' * 10
+artist.events.each { |e| puts "#{e.venue.city} at #{e.venue.name} on #{e.start_date.strftime("%D")}" }
 
 puts
 
