xenda/rockstar

commit f2a291f36fffeaaeb48a9f8ff1abe9bac71dc2d3

Allowing artist to be looked up by mbid

lib/rockstar/artist.rb
@@ -77,9 +77,11 @@ module Rockstar
       end
     end
     
-    def initialize(name, o={})
-      raise ArgumentError, "Name is required" if name.blank?
-      @name = name
+    def initialize(o={})
+      raise ArgumentError, "Name or mbid is required" if o[:name].nil? && o[:mbid].nil?
+      
+      @name = o[:name] unless o[:name].nil?
+      @mbid = o[:mbid] unless o[:mbid].nil?
 
       options = {:include_info => false}.merge(o)
       load_info if options[:include_info]
@@ -87,7 +89,9 @@ module Rockstar
 
     def load_info(xml=nil)
       unless xml
-        doc = self.class.fetch_and_parse("artist.getInfo", {:artist => @name})
+        params = @mbid.nil? ? {:artist => @name} : {:mbid => @mbid}
+        
+        doc = self.class.fetch_and_parse("artist.getInfo", params)
         xml = (doc / :artist).first
       end
 

commit 69368873a646d2c1b4e0de4e06f9d00ff5037529

Fixed errors that where introduced by commit f2a291f36fffeaaeb48a

lib/rockstar/artist.rb
@@ -77,10 +77,9 @@ module Rockstar
       end
     end
     
-    def initialize(o={})
-      raise ArgumentError, "Name or mbid is required" if o[:name].nil? && o[:mbid].nil?
-      
-      @name = o[:name] unless o[:name].nil?
+    def initialize(name, o={})
+      raise ArgumentError, "Name or mbid is required" if name.blank? && o[:mbid].blank?
+      @name = name
       @mbid = o[:mbid] unless o[:mbid].nil?
 
       options = {:include_info => false}.merge(o)