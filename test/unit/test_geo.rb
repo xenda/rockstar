accb14abb9a550c3ed9b373fe07018684c246ca4
rob-murray <robmurray17@gmail.com>
2012-12-27 18:47:36 +0000

    Added tests for geo.topartists and geo.toptracks methods

--- test/unit/test_geo.rb
+++ test/unit/test_geo.rb
@@ -21,5 +21,31 @@ class TestGeo < Test::Unit::TestCase
     assert_equal("Germany", metros.first.country)
   end
   
+  test "should get top artists for a country" do
+    limit = 50 #the mock resultset has 50 in so just fake this
+    
+    geo = Rockstar::Geo.new
+    
+    artists = geo.topartists("spain", limit)
+    assert_equal(limit, artists.size)
+    assert_equal("Muse", artists.first.name)
+    assert_equal("1695c115-bf3f-4014-9966-2b0c50179193", artists.first.mbid)
+    assert_equal('1736', artists.first.listenercount)
+    
+  end
+  
+  test "should get top tracks for a country" do
+    limit = 50 #the mock resultset has 50 in so just fake this
+    
+    geo = Rockstar::Geo.new
+    
+    tracks = geo.toptracks("spain", limit)
+    assert_equal(limit, tracks.size)
+    assert_equal("Diamonds", tracks.first.name)
+    assert_equal("Rihanna", tracks.first.artist)
+    assert_equal("f281056d-5696-43d0-bb2e-61677ef65309", tracks.first.mbid)
+    
+  end  
+  
 
 end
